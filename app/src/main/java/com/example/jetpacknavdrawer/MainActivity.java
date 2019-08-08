05a5e5c7c192bd862a4c6ff4a7f2cb0ba88f5893
Parag Dangriya <45040382+ParagsDangariya@users.noreply.github.com>
2019-08-08 15:50:47 -0400

    half

--- app/src/main/java/com/example/jetpacknavdrawer/MainActivity.java
+++ app/src/main/java/com/example/jetpacknavdrawer/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.jetpacknavdrawer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    public Toolbar toolbar;
+    public DrawerLayout drawerLayout;
+    public NavController navController;
+    public NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+    public void setupNavigation(){
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawerlayout);
+
+        navigationView = findViewById(R.id.navigationview);
+
+        navController = Navigation.findNavController(this,R.id.host_fragment);
+
+        //NavigationUI.setupActionBarWithNavController(this,);
     }
 }
