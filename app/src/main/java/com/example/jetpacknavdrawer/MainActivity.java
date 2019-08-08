953380f629178ef44f7032a9cefa5c0f31679f1d
Parag Dangriya <45040382+ParagsDangariya@users.noreply.github.com>
2019-08-08 16:49:42 -0400

    full

--- app/src/main/java/com/example/jetpacknavdrawer/MainActivity.java
+++ app/src/main/java/com/example/jetpacknavdrawer/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jetpacknavdrawer;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 
@@ -21,17 +22,41 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public NavController navController;
     public NavigationView navigationView;
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(Navigation.findNavController(this,R.id.host_fragment),drawerLayout);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupNavigation();
 
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        menuItem.setCheckable(true);
+
+        drawerLayout.closeDrawers();
+
+        int id = menuItem.getItemId();
+
+        switch (id){
+            case R.id.first:
+                navController.navigate(R.id.first);
+                break;
+            case R.id.second:
+                navController.navigate(R.id.second);
+                break;
+            case R.id.third:
+                navController.navigate(R.id.third);
+                break;
+
+        }
         return false;
+
     }
     public void setupNavigation(){
         toolbar = findViewById(R.id.toolbar);
@@ -45,6 +70,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         navController = Navigation.findNavController(this,R.id.host_fragment);
 
-        //NavigationUI.setupActionBarWithNavController(this,);
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout);
+        NavigationUI.setupWithNavController(navigationView,navController);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+
+
+            super.onBackPressed();
+        }
     }
 }
